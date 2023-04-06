07193d9584aef4f0968ba36d93fd5609bf4e9a36
Your Namemoshenh01exit[200~git config --global user.name Your <moshenh01@gmail.com>
2023-04-07 00:17:07 +0300

    fix mum 2

--- StudentTest1.cpp
+++ StudentTest1.cpp
@@ -91,16 +91,17 @@ TEST_CASE("Printing the winner")
 
 TEST_CASE("The game ends after at most 26 turns")
 {
+   
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
-    bool maxTurns = 26;
-    
-    for (int i = 0 ; i < 26 && p1.stacksize()>0; i++ )
+    int maxTurns = 26;
+    int i = 0;
+    for ( ; i < 26 && p1.stacksize()>0; i++ )
     {
         game.playTurn();
     }
-    CHECK(maxTurns == true);
+    CHECK(maxTurns >= i );
     CHECK(p1.stacksize() == 0);
     CHECK(p2.stacksize() == 0);
 }
